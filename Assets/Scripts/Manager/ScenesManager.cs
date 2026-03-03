48a23912a1472855f54842b2a9240a82713747b0
Haningyu <haranharan1105@gmail.com>
2026-03-03 23:50:24 +0900

    씬매니저 관리 로직 변경

--- Assets/Scripts/Manager/ScenesManager.cs
+++ Assets/Scripts/Manager/ScenesManager.cs
@@ -1,25 +1,64 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+public enum SceneId
+{
+    Title,
+    Lobby,
+    Game,
+    Result
+}
+
+
+[Serializable]
+public class SceneEntry
+{
+    public SceneId id;
+    public string sceneName;
+}
+
+
 public class ScenesManager : MonoBehaviour
 {
-    public void LoadRoom()
+    public static ScenesManager Instance;
+
+    [SerializeField] private List<SceneEntry> scenes = new();
+
+    private Dictionary<SceneId, string> sceneMap;
+
+    void Awake()
     {
-        SceneManager.LoadScene("RoomScene");
-    }
-    public void LoadGame()
-    {
-        SceneManager.LoadScene("GameScene");
-    }
-    public void LoadCoice()
-    {
-        SceneManager.LoadScene("CoiceScene");
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        sceneMap = new Dictionary<SceneId, string>();
+
+        for (int i = 0; i < scenes.Count; i++)
+            sceneMap.Add(scenes[i].id, scenes[i].sceneName);
     }
 
-    public void LoadTitle()
+    public void Load(SceneId id)
     {
-        SceneManager.LoadScene("TitleScene");
+        if (!sceneMap.TryGetValue(id, out var name))
+        {
+            Debug.LogError("씬이 없습니다: " + id);
+            return;
+        }
+
+        SceneManager.LoadScene(name);
+    }
+
+    public void ReloadCurrent()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
