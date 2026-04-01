fa407c743c6e9b2f656d14225cd3ee73f85be9b7
Haningyu <haranharan1105@gmail.com>
2026-04-02 00:39:44 +0900

    데미지 텍스트 풀반환 버그 수정 및 프리팹 적용

--- Assets/Scripts/UI/DamageText.cs
+++ Assets/Scripts/UI/DamageText.cs
@@ -22,6 +22,6 @@ public class DamageText : MonoBehaviour
 
         timer += Time.deltaTime;
         if (timer >= lifeTime)
-            gameObject.SetActive(false);
+            GetComponent<PooledObject>().ReturnToPool();
     }
 }
